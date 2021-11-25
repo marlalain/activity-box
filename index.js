1789c91bfd4e672fb4c35233207ddef25a257a19
Paulo Elienay <paulo@pauloelienay.com>
2021-11-25 06:05:56 -0300

    fix: revert Twitter init

--- index.js
+++ index.js
@@ -37,14 +37,6 @@ Toolkit.run(
       TWITTER_HANDLER,
     } = process.env
 
-    // Twitter integration
-    let client = new Twitter({
-      consumer_key: TWITTER_CONSUMER_KEY,
-      consumer_secret: TWITTER_CONSUMER_SECRET,
-      access_token_key: TWITTER_ACCESS_TOKEN_KEY,
-      access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
-    })
-
     // Get the user's public events
     tools.log.debug(`Getting activity for ${GH_USERNAME}`)
     const events = await tools.github.activity.listPublicEventsForUser({
